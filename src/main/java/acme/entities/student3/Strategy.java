fc15af0c84272c2c7b2f1f5c24f5d36ed10d4123
Flamyon <franrodriguezcarretero@gmail.com>
2026-02-22 20:56:11 +0100

    Added entities for student 3

---
+++ src/main/java/acme/entities/student3/Strategy.java
@@ -0,0 +1,96 @@
+
+package acme.entities.student3;
+
+import java.time.Duration;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoment.Constraint;
+import acme.client.components.validation.ValidUrl;
+import acme.client.helpers.MomentHelper;
+import acme.client.helpers.SpringHelper;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import acme.constraints.ValidTicker;
+import acme.realms.Fundraiser;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Strategy extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidTicker
+	@Column(unique = true)
+	private String				ticker;
+
+	@Mandatory
+	@ValidHeader
+	@Column
+	private String				name;
+
+	@Mandatory
+	@ValidText
+	@Column
+	private String				description;
+
+	@Mandatory
+	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Moment				startMoment;
+
+	@Mandatory
+	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Moment				endMoment;
+
+	@Optional
+	@ValidUrl
+	@Column
+	private String				moreInfo;
+
+	@Mandatory
+	@Valid
+	@Column
+	private Boolean				draftMode;
+
+	// Derived attributes
+
+
+	@Transient
+	public Double getMonthsActive() {
+		Duration duration = MomentHelper.computeDuration(this.startMoment, this.endMoment);
+		double months = duration.toDays() / 30.;
+		return Math.round(months * 10) / 10.0;
+	}
+
+	@Transient
+	public Double getExpectedPercentage() {
+		StrategyRepository repository;
+		repository = SpringHelper.getBean(StrategyRepository.class);
+		return repository.expectedPercentage(this.getId());
+	};
+	// DB relation
+
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Fundraiser fundraiser;
+
+}
