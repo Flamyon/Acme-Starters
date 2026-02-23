02229f225ee4babddaf94322d2b725f543d234dd
Alvar0j <alvarojimenezosuna03@gmail.com>
2026-02-23 18:45:37 +0100

    fix: atributos calculados y pequeñas correcciones

--- src/main/java/acme/entities/student4/Sponsorship.java
+++ src/main/java/acme/entities/student4/Sponsorship.java
@@ -1,6 +1,7 @@
 
 package acme.entities.student4;
 
+import java.time.Duration;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -8,14 +9,19 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoment.Constraint;
 import acme.client.components.validation.ValidUrl;
+import acme.client.helpers.MomentHelper;
 import acme.constraints.ValidHeader;
 import acme.constraints.ValidText;
 import acme.constraints.ValidTicker;
@@ -29,64 +35,94 @@ import lombok.Setter;
 public class Sponsorship extends AbstractEntity {
 
 	//Serialisation version --------------------------------------------------
-	private static final long	serialVersionUID	= 1L;
+	private static final long		serialVersionUID	= 1L;
 
 	//Attributes -------------------------------------------------------------
 
 	@Mandatory
 	@ValidTicker
 	@Column(unique = true)
-	private String				ticker;
+	private String					ticker;
 
 	@Mandatory
 	@ValidHeader
 	@Column
-	private String				name;
+	private String					name;
 
 	@Mandatory
 	@ValidText
 	@Column
-	private String				description;
+	private String					description;
 
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
 	@Temporal(TemporalType.TIMESTAMP) //Almacenará algo como 2023-10-25 15:30:00.000.
-	private Date				startMoment;
+	private Date					startMoment;
 
 	@Mandatory
 	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date				endMoment;
+	private Date					endMoment;
 
 	@Optional
 	@ValidUrl
 	@Column
-	private String				moreInfo;
+	private String					moreInfo;
 
 	@Mandatory
 	@Valid
 	@Column
-	private Boolean				draftMode;
+	private Boolean					draftMode;
+
+	// Derived attributes -----------------------------------------------------
+
+	@Transient
+	@Autowired
+	private SponsorshipRepository	sponsorshipRepository;
 
 	/*
 	 * totalMoney: - The total money of a
 	 * sponsorship is the sum of money in
 	 * the corresponding donations. Only
 	 * Euros are accepted.
-	 * 
+	 */
+
+
+	@Transient
+	public Money getTotalMoney() {
+		Money totalMoney = new Money();
+		totalMoney.setCurrency("EUR");
+
+		Double res;
+		res = this.sponsorshipRepository.calculateTotalMoney(this.getId());
+
+		if (res == null)
+			totalMoney.setAmount(0.0);
+		else
+			totalMoney.setAmount(res);
+		return totalMoney;
+	}
+
+	/*
 	 * - monthsActive is computed as the
 	 * number of months in interval
 	 * startMoment/endMoment rounded to the
 	 * nearest decimal.
 	 */
 
-	// Derived attributes -----------------------------------------------------
+	@Transient
+	public Double getMonthsActive() {
+		Duration duration = MomentHelper.computeDuration(this.startMoment, this.endMoment);
+		double months = duration.toDays() / 30.;
+		return Math.round(months * 10) / 10.;
+	}
 
 	// Relationships ----------------------------------------------------------
+
 
 	@Mandatory
 	@Valid
 	@ManyToOne(optional = false)
-	private Sponsor				sponsor;
+	private Sponsor sponsor;
 
 }
