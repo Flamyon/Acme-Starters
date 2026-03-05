f251b6745bcb4876707f1475b26943dff3d33704
Alvar0j <alvarojimenezosuna03@gmail.com>
2026-03-05 16:43:07 +0100

    fix: validator & populators

--- src/main/java/acme/constraints/SponsorshipValidator.java
+++ src/main/java/acme/constraints/SponsorshipValidator.java
@@ -27,34 +27,39 @@ public class SponsorshipValidator extends AbstractValidator<ValidSponsorship, Sp
 
 	@Override
 	public boolean isValid(final Sponsorship sponsorship, final ConstraintValidatorContext context) {
-
 		assert context != null;
 
 		if (sponsorship == null)
 			return true;
-
-		if (sponsorship.getTicker() != null) {
+		{
 			Sponsorship existingSponsorship = this.repository.findSponsorshipByTicker(sponsorship.getTicker());
+			boolean uniqueSponsorship = existingSponsorship == null || existingSponsorship.getId() == sponsorship.getId();
 
-			boolean uniqueTicker = existingSponsorship == null || existingSponsorship.getId() == sponsorship.getId();
-
-			super.state(context, uniqueTicker, "ticker", "acme.validation.sponsorship.ticker.non-unique");
+			super.state(context, uniqueSponsorship, "ticker", "acme.validation.sponsorship.duplicated-ticker.message");
 		}
 
 		if (sponsorship.getDraftMode() != null && !sponsorship.getDraftMode()) {
 
-			Integer donationsCount = this.repository.countDonationsBySponsorshipId(sponsorship.getId());
-			boolean hasDonations = donationsCount != null && donationsCount >= 1;
+			{
+				Date now = MomentHelper.getBaseMoment();
+				Date start = sponsorship.getStartMoment();
+				Date end = sponsorship.getEndMoment();
 
-			super.state(context, hasDonations, "draftMode", "acme.validation.sponsorship.donations.error");
+				if (start != null && end != null) {
+					boolean correctOrder = end.after(start);
+					super.state(context, correctOrder, "endMoment", "acme.validation.sponsorship.invalid-date-order.message");
 
-			Date now = MomentHelper.getBaseMoment();
-			Date start = sponsorship.getStartMoment();
-			Date end = sponsorship.getEndMoment();
+					boolean futureStart = start.after(now);
+					super.state(context, futureStart, "startMoment", "acme.validation.sponsorship.date-not-future.message");
+				}
+			}
 
-			boolean validDates = start != null && end != null && !start.before(now) && end.after(start);
+			{
+				Integer existingDonations = this.repository.countDonationsBySponsorshipId(sponsorship.getId());
+				boolean hasDonation = existingDonations != null && existingDonations > 0;
 
-			super.state(context, validDates, "startMoment", "acme.validation.sponsorship.dates.error");
+				super.state(context, hasDonation, "draftMode", "acme.validation.sponsorship.missing-donations.message");
+			}
 		}
 
 		return !super.hasErrors(context);
