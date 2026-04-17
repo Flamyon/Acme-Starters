df481d3469b9d364dfebbc26f2e18a402f907cd9
Flamyon <franrodriguezcarretero@gmail.com>
2026-04-17 18:09:06 +0200

    fix update y create de componentes

--- src/main/java/acme/features/student3/fundraiser/strategy/FundraiserStrategyCreateService.java
+++ src/main/java/acme/features/student3/fundraiser/strategy/FundraiserStrategyCreateService.java
@@ -42,8 +42,8 @@ public class FundraiserStrategyCreateService extends AbstractService<Fundraiser,
 		int projectId;
 		Project project;
 
-		projectId = super.getRequest().getData("project", int.class);
-		project = this.repo.findProjectById(projectId);
+		projectId = super.getRequest().hasData("project", int.class) ? super.getRequest().getData("project", int.class) : 0;
+		project = projectId == 0 ? null : this.repo.findProjectById(projectId);
 		this.entity.setProject(project);
 
 		super.bindObject(this.entity, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo");
@@ -52,10 +52,16 @@ public class FundraiserStrategyCreateService extends AbstractService<Fundraiser,
 	@Override
 	public void validate() {
 		boolean validProject;
+		Long memberships;
 
 		super.validateObject(this.entity);
 
-		validProject = this.entity.getProject() != null && this.repo.countDraftMembershipByProjectAndUserAccountId(this.entity.getProject().getId(), this.entity.getFundraiser().getUserAccount().getId()) > 0;
+		if (this.entity.getProject() == null)
+			validProject = true;
+		else {
+			memberships = this.repo.countDraftMembershipByProjectAndUserAccountId(this.entity.getProject().getId(), this.entity.getFundraiser().getUserAccount().getId());
+			validProject = memberships != null && memberships > 0;
+		}
 		super.state(validProject, "project", "fundraiser.strategy.form.error.project");
 
 		// 1. ticker uniqueness (prevent DB unique-constraint errors on create)
